06cfb43b915229612f03ba4f961c75f771d03558
Clay Stewart <claytonmstewart@gmail.com>
2016-04-17 11:46:44 -0500

    add scroll to top on state change

--- app/routes.js
+++ app/routes.js
@@ -68,4 +68,8 @@ angular.module('capFinPlanning').config([
 
     $urlRouterProvider.otherwise('/home');
 
+}]).run(['$rootScope', function($rootScope) {
+  $rootScope.$on('$stateChangeSuccess', function() {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+  });
 }]);
